a2a57a0a3414563fa9122f9ea68cd2721bba8760
llabuhn <la.labuhn@gmail.com>
2026-06-03 17:07:12 +0200

    Update downgradeReso.py
    - Rewrites downgradereso as a single numpy-only function
    - Replaces shrink_sum/max/min/average with one vectorized block-aggregation
    - Nodata (-999) handled via NaN + np.nan*.

--- downgradeReso.py
+++ downgradeReso.py
@@ -1,131 +1,24 @@
-import numpy as np 
-import matplotlib.pyplot as plt
+"""
+=========================================================================
+Downgrade 2D Array Resolution 
 
-######################################################
-def downgrade_resolution_4nadir(arr, diag_res_cte_shape, flag_interpolation='conservative'):
+- Reduces a 2D array to a target resolution using block aggregation.
+- Methods: 'average', 'min', 'max', 'sum' (ignore nodata), 'conservative' (nodata=0).
+- Nodata (-999) is internally converted to NaN.
+=========================================================================
+"""
 
-    '''
-    flag_interpolation is conservative, max, min, average or sum 
-    '''
-    factor = (1.*arr.shape[0]/diag_res_cte_shape[0])
-    if factor == int(np.floor(factor)): factor = int(factor)
-    else: factor = int(factor) + 1
-    #factor = int(np.round(old_div(1.*arr.shape[0],diag_res_cte_shape[0]),0))
-    
-    if np.mod( arr.shape[0], factor )!=0:
-        extra_pixel0 = factor-np.mod( arr.shape[0], factor )
-        extra_pixel1 = factor-np.mod( arr.shape[1], factor )
-    else: 
-        extra_pixel0 = 0
-        extra_pixel1 = 0
-  
-    if (extra_pixel0>0) |  (extra_pixel1>0):
-        x = np.arange(0,arr.shape[0],1)
-        y = np.arange(0,arr.shape[1],1)
-        z = arr.flatten()
-        f = interpolate.interp2d(x, y, z, kind='linear')
-        
-        grid_x = np.arange(0-int(0.5*extra_pixel0),extra_pixel0-int(0.5*extra_pixel0)+arr.shape[0],1)
-        grid_y = np.arange(0-int(0.5*extra_pixel1),extra_pixel1-int(0.5*extra_pixel1)+arr.shape[1],1) 
-        arr = f(grid_x, grid_y)
-        arr = arr.T
-    
-    if flag_interpolation == 'max':
-        return shrink_max(arr, diag_res_cte_shape[0], diag_res_cte_shape[1])
-   
-    elif flag_interpolation == 'min':
-        return shrink_min(arr, diag_res_cte_shape[0], diag_res_cte_shape[1])
-    
-    elif flag_interpolation == 'conservative':
-        
-        mask = np.where(arr!=-999, 1, 0)
-        sum_pixel = shrink_sum(mask, diag_res_cte_shape[0], diag_res_cte_shape[1]) 
-        
-        sum = shrink_sum(arr, diag_res_cte_shape[0], diag_res_cte_shape[1]) 
-        return np.where(sum != -999, (sum/sum_pixel), sum)
+import numpy as np
 
-    elif flag_interpolation == 'average':
-        return shrink_average(arr, diag_res_cte_shape[0], diag_res_cte_shape[1])
-    
-    elif flag_interpolation == 'sum':
-        return shrink_sum(arr, diag_res_cte_shape[0], diag_res_cte_shape[1])
+def downgrade_resolution_4nadir(a, out_shape, flag_interpolation, nodata=-999):
+    a = np.where(a == nodata, np.nan, a)
+    reducer = {
+        'min': np.nanmin,
+        'max': np.nanmax,
+        'sum': np.nansum,
+        'conservative': lambda x, axis: np.mean(np.nan_to_num(x, nan=0.0), axis=axis)
+    }.get(flag_interpolation, np.nanmean)   # fallback np.nanmean == 'average'
 
-    else:
-        print('bad flag')
-        pdb.set_trace()
-
-
-######################################################
-def shrink_sum(data, nx, ny, nodata=-999):
-    
-    data_masked = np.ma.array(data, mask = np.where(data==nodata,1,0))
-    nnx, nny    = data_masked.shape
-
-    # Reshape data
-    rshp = data_masked.reshape([nx, nnx//nx, ny, nny//ny])
-
-    # Compute mean along axis 3 and remember the number of values each mean
-    # was computed from
-    return np.where(rshp.sum(3).sum(1).mask==False, rshp.sum(3).sum(1).data, nodata)
-
-######################################################
-def shrink_max(data, nx, ny, nodata=-999):
-    data_masked = np.ma.array(data, mask = np.where(data==nodata,1,0))
-    nnx, nny    = data_masked.shape
-
-    # Reshape data
-    rshp = data_masked.reshape([nx, nnx//nx, ny, nny//ny])
-
-    # Compute mean along axis 3 and remember the number of values each mean
-    # was computed from
-    return np.where(rshp.max(3).max(1).mask==False, rshp.max(3).max(1).data, nodata)
-    
-    #return min3    return data.reshape(rows, data.shape[0]/rows, cols, data.shape[1]/cols).max(axis=1).max(axis=2)
-
-
-######################################################
-def shrink_min(data, nx, ny, nodata=-999):
-    
-    data_masked = np.ma.array(data, mask = np.where(data==nodata,1,0))
-    nnx, nny    = data_masked.shape
-
-    # Reshape data
-    rshp = data_masked.reshape([nx, nnx//nx, ny, nny//ny])
-
-    # Compute mean along axis 3 and remember the number of values each mean
-    # was computed from
-    return np.where(rshp.min(3).min(1).mask == False, rshp.min(3).min(1).data, nodata)
-
-    #return data.reshape(rows, data.shape[0]/rows, cols, data.shape[1]/cols).min(axis=1).min(axis=2)
-
-
-######################################################
-def shrink_average(data, nx, ny, nodata=-999.):
-   
-    data_masked = np.ma.array(data, mask = np.where(data==nodata, 1, 0))
-    nnx, nny    = data_masked.shape
-
-    # Reshape data
-    rshp = data_masked.reshape([nx, nnx//nx, ny, nny//ny])
-
-    # Compute mean along axis 3 and remember the number of values each mean
-    # was computed from
-    mean3 = rshp.mean(3)
-    count3 = rshp.count(3)
-
-    # Compute weighted mean along axis 1
-    mean1 = ((count3*mean3).sum(1)/count3.sum(1))
-    
-    return np.where( mean1.mask, nodata, mean1.data)
-
-
-if __name__ == '__main__':
-    arr = np.random.random([10,10])
-    mm = downgrade_resolution_4nadir(arr, [5,5], flag_interpolation='sum')
-
-    ax = plt.subplot(121)
-    plt.imshow(arr.T, origin='lower')
-    ax = plt.subplot(122)
-    plt.imshow(mm.T, origin='lower')
-    plt.show()
-
+    bx, by = a.shape[0] // out_shape[0], a.shape[1] // out_shape[1]
+    a = a[:bx*out_shape[0], :by*out_shape[1]]
+    return reducer(a.reshape(out_shape[0], bx, out_shape[1], by), axis=(1,3))
